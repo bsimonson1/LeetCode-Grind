13f86593266ee0feeff6ca1e7c1727eb68077cae
Ben <100646537+bsimonson1@users.noreply.github.com>
2024-08-09 08:37:23 -0400

    Time: 46 ms (6.47%), Space: 37.5 MB (28.8%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -9,30 +9,34 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ #include <iostream>
 class Solution {
 public:
-    void recur(TreeNode* root, std::vector<int>& vals)
+    // void recur(TreeNode* root, std::vector<int>& vals)
+    // {
+    //     if (root != NULL)
+    //     {
+    //         vals.push_back(root->val);
+    //         recur(root->left, vals);
+    //         recur(root->right, vals);
+    //     }
+    // }
+    bool recur(TreeNode* root, int k, std::set<int>& vals)
     {
-        if (root != NULL)
+        if (root == NULL)
         {
-            vals.push_back(root->val);
-            recur(root->left, vals);
-            recur(root->right, vals);
+            return false;
         }
+        if (vals.count(k - root->val))
+        {
+            return true;
+        }
+        vals.insert(root->val);
+        
+        return recur(root->left, k, vals) || recur(root->right, k, vals);
     }
     bool findTarget(TreeNode* root, int k) {
-        std::vector<int> vals;
-        recur(root, vals);
-        for (int i = 0; i < vals.size() - 1; i++)
-        {
-            for (int j = i + 1; j < vals.size(); j++)
-            {
-                if (vals[i] + vals[j] == k)
-                {
-                    return true;
-                }
-            }
-        }
-        return false;
+        std::set<int> vals;
+        return recur(root, k, vals);
     }
 };
