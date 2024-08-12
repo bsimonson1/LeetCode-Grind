834c62d72507d27ceaa83f8c42b106de15bfbce5
Ben <100646537+bsimonson1@users.noreply.github.com>
2024-08-12 12:20:32 -0400

    Time: 24 ms (73.5%), Space: 24.8 MB (39.1%) - LeetHub

---
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+class KthLargest {
+public:
+    std::priority_queue<int, std::vector<int>, std::greater<int>> minH;
+    int k;
+    KthLargest(int k, vector<int>& nums) : k(k) {
+        for (auto& num : nums)
+        {
+            minH.push(num);
+            if (minH.size() > k)
+            {
+                minH.pop();
+            }
+        }
+    }
+    
+    int add(int val) {
+        minH.push(val);
+        if (minH.size() > k)
+        {
+            minH.pop();
+        }
+        return minH.top();
+        // stream.push_back(val);
+        // std::vector<int> copy;
+        // copy = stream;
+        // sort(copy.begin(), copy.end(), std::greater<int>());
+        // int low = 0;
+        // int high = copy.size();
+        // while (low <= high) 
+        // {
+        //     int mid = low + (high - low) / 2;
+
+        //     if (dest == (mid+1))
+        //         return copy[mid];
+
+        //     if (mid < dest)
+        //         low = mid + 1;
+
+        //     else
+        //         high = mid - 1;
+        // }
+        // return -1;
+    }
+};
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
