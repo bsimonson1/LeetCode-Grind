d4d651771ea57dd7ee0e761e343595ae5e7fb772
Ben <100646537+bsimonson1@users.noreply.github.com>
2024-08-12 15:36:28 -0400

    Time: 5 ms (50.25%), Space: 9.3 MB (43.82%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,29 +1,12 @@
 
 class Solution {
 public:
-    string binaryConversion(int n)
-    {
-        if (n == 0) return "0";
-        std::string binary;
-        while (n > 0)
-        {
-            if (n % 2 == 0)
-            {
-                binary = "1" + binary;;
-            }
-            else
-            {
-                binary = "0" + binary;
-            }
-            n /= 2;
-        }
-        return binary;
-    }
+
     vector<int> countBits(int n) {  
-        std::vector<int> bin;
+        std::vector<int> bin(n+1);
         for (int i = 0; i <= n; i++)
         {
-            bin.push_back(__builtin_popcount(i));
+            bin[i] = (__builtin_popcount(i));
         }
         return bin;
     }
