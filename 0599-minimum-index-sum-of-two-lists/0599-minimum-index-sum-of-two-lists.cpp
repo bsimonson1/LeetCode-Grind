438d1b86004f1034c6df537c74699b1cc062c2da
Ben <100646537+bsimonson1@users.noreply.github.com>
2024-08-13 12:46:28 -0400

    Time: 62 ms (89.8%), Space: 40.9 MB (38.29%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
@@ -3,36 +3,20 @@ class Solution {
 public:
     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
         std::unordered_map<std::string, int> mp1;
-        std::unordered_map<std::string, int> mp2;
         int lowest = INT_MAX;
-        for (int i = 0; i < list1.size(); i++)
-        {
-            mp1[list1[i]] = i;
-        }
+        std::vector<std::string> same;
+        for (int i = 0; i < list1.size(); i++) mp1[list1[i]] = i;
         for (int i = 0; i < list2.size(); i++)
         {
-            mp2[list2[i]] = i;
-        }
-        for (auto& i : mp1)
-        {
-            std::cout<<i.first<< " "<<i.second<<std::endl;
-            if (mp2.find(i.first) != mp2.end())
+            if ((mp1.find(list2[i]) != mp1.end()) && (mp1.at(list2[i]) + i) < lowest)
             {
-                if ((i.second + mp2.at(i.first)) < lowest)
-                {
-                    lowest = (i.second + mp2.at(i.first));
-                }
+                lowest = mp1.at(list2[i]) + i;
+                same.clear();
+                same.push_back(list2[i]);
             }
-        }
-        std::vector<std::string> same;
-        for (auto& i : mp1)
-        {
-            if (mp2.find(i.first) != mp2.end())
+            else if ((mp1.find(list2[i]) != mp1.end()) && (mp1.at(list2[i]) + i) == lowest)
             {
-                if ((i.second + mp2.at(i.first)) == lowest) 
-                {
-                    same.push_back(i.first);
-                }
+                same.push_back(list2[i]);
             }
         }
         return same;
